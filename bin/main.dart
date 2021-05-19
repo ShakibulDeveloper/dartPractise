da91e20a5c9d2d376b6a6b023172fc5b84191f24
ShakibulDeveloper <epiktheme@gmail.com>
2021-05-20 04:01:41 +0600

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,50 @@
+void main() {
+  //dart starting tag
+
+  //variable types
+
+  /* 
+    01. Number
+      -> int(integer) = non fractional number 
+      -> double = fractional number
+    02. String = texts
+    03. Boolen = ether true or false value
+    04.list & Map
+    list = multiple object hold (array)
+    Map = values with specfic keys
+
+  */
+
+  //variable without specific...
+  var text = "Hello World";
+  var number = 10;
+
+  print(text);
+  print(number);
+
+  //variable with specific...
+  String sText = "Hello World";
+  int sNumber = 10;
+
+  print(sText);
+  print(sNumber);
+
+  // variable concatenation in dart
+  var firstName = "Shakibul";
+  var lastName = "Alam";
+
+  print(firstName + ' ' + lastName);
+
+  // add, sub, multiplication, division
+
+  int x = 10;
+  int y = 5;
+  int z = 20;
+
+  print(x + y + z);
+  print(x + y - z);
+  print((x * y) + z);
+  print((x + y) / z);
+
+//dart end tag
+}
